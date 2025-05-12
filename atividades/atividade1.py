1c2268d94ca3ed04d74d62755b0e632875bc1787
donilindo <donavan_lopes@estudante.sesisenai.org.br>
2025-05-12 13:07:38 +0000

    update

--- atividades/atividade1.py
+++ atividades/atividade1.py
@@ -1,5 +1,10 @@
-c = float(input("Temperatura em Celsius: "))
-f = (c * 9/5) + 32
-print(f"Temperatura em Fahrenheit:  {f}")
+quantidade = int(input("Insira a quantidade de sandwiches que deseja fazer: "))
 
+presunto_queijo = 0.05
+hamburguer = 0.1
 
+calculo = quantidade * presunto_queijo
+calculo2 = quantidade * hamburguer
+
+print(f"A quantidade para comprar de hamburguer é: {calculo2}Kg")
+print(f"A quantidade para comprar de presunto e queijo é {calculo}Kg")
